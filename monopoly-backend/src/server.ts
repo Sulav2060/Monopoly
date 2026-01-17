5bd199518dc3a4624176cb5933d290d182f89ae5
sulav2060 <sulav2060@gmail.com>
2026-01-17 21:59:41 +0545

    add game management by adding lastActive tracking and cleanup functionality

--- monopoly-backend/src/server.ts
+++ monopoly-backend/src/server.ts
@@ -5,11 +5,13 @@ import app from "./app";
 import { createServer } from "http";
 import { WebSocketServer } from "ws";
 import { setupWebSocket } from "./ws/handler";
+import { startGameCleanup } from "./ws/gameStore";
 
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
 
 setupWebSocket(wss);
+startGameCleanup();
 
 const PORT = process.env.PORT || 4000;
 

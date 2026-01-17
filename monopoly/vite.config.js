961ef32bc6de7afaafd21686945077107cbdfe23
sulav2060 <sulav2060@gmail.com>
2026-01-17 22:47:06 +0545

    fix(build): remove manualChunks to resolve React 19 production build crash

--- monopoly/vite.config.js
+++ monopoly/vite.config.js
@@ -5,31 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  //vendor-three.js dherai heavy vayera chunk ma break gareko
-  //with this, change in index.js wont load whole three.js again (1.5mb) but only our code
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('three-stdlib')) {
-              return 'vendor-three-stdlib';
-            }
-            if (id.includes('@react-three/drei')) {
-              return 'vendor-react-three-drei';
-            }
-            if (id.includes('@react-three/fiber')) {
-              return 'vendor-react-three-fiber';
-            }
-            if (id.includes('three')) {
-              return 'vendor-three';
-            }
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react';
-            }
-            return 'vendor';
-          }
-        },
+        // manualChunks removed to fix React 19 production build crash
+        //need to find a better solution later
       },
     },
   },
